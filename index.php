03d9cd6e0b20482cc7874d8c74bcf25e0588a13e
David_Hamelin <hamelindavid@hotmail.fr>
2019-08-22 15:22:46 +0200

    indentation

--- index.php
+++ index.php
@@ -10,63 +10,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Super RSS Reader</title>
   </head>
+
   <body>
-    <header><h1 class="center-align teal-text">Super RSS Reader</h1></header>
+
+    <header>
+      <h1 class="center-align teal-text">Super RSS Reader</h1>
+    </header>
 
     <nav>
-    <div class="nav-wrapper container">
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="sass.html">Sécurité</a></li>
-        <li><a href="badges.html">Applis, Logiciels</a></li>
-        <li><a href="collapsible.html">Jeux</a></li>
-      </ul>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Nom Prénom</a></li>
-        <li><a href="badges.html">Paramètres</a></li>
-      </ul>
-    </div>
-  </nav>
+      <div class="nav-wrapper container">
+        <ul id="navSujet" class="left hide-on-med-and-down">
+          <li><a href="sass.html">Sécurité</a></li>
+          <li><a href="badges.html">Applis, Logiciels</a></li>
+          <li><a href="collapsible.html">Jeux</a></li>
+        </ul>
+        <ul id="navParams" class="right hide-on-med-and-down">
+          <li><a href="sass.html">Nom Prénom</a></li>
+          <li><a href="badges.html">Paramètres</a></li>
+        </ul>
+      </div>
+    </nav>
 
   <section class="row container">
+
     <article class="col s4">
-    <ul class="collection">
-      <li class="collection-item avatar">
-        <img src="images/yuna.jpg" alt="" class="circle">
-        <span class="title">Title</span>
-        <p>First Line <br>
-          Second Line
-        </p>
-        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-      </li>
-      <li class="collection-item avatar">
-        <i class="material-icons circle">folder</i>
-        <span class="title">Title</span>
-        <p>First Line <br>
-          Second Line
-        </p>
-        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-      </li>
-      <li class="collection-item avatar">
-        <i class="material-icons circle green">insert_chart</i>
-        <span class="title">Title</span>
-        <p>First Line <br>
-          Second Line
-        </p>
-        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-      </li>
-      <li class="collection-item avatar">
-        <i class="material-icons circle red">play_arrow</i>
-        <span class="title">Title</span>
-        <p>First Line <br>
-          Second Line
-        </p>
-        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-      </li>
-    </ul>      
+      <ul class="collection">
+        <li class="collection-item avatar">
+          <img src="images/yuna.jpg" alt="" class="circle">
+          <span class="title">Title</span>
+          <p>First Line <br>
+            Second Line
+          </p>
+          <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </li>
+        <li class="collection-item avatar">
+          <i class="material-icons circle">folder</i>
+          <span class="title">Title</span>
+          <p>First Line <br>
+            Second Line
+          </p>
+          <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </li>
+        <li class="collection-item avatar">
+          <i class="material-icons circle green">insert_chart</i>
+          <span class="title">Title</span>
+          <p>First Line <br>
+            Second Line
+          </p>
+          <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </li>
+        <li class="collection-item avatar">
+          <i class="material-icons circle red">play_arrow</i>
+          <span class="title">Title</span>
+          <p>First Line <br>
+            Second Line
+          </p>
+          <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </li>
+      </ul>      
     </article>
 
     <article class="col s4">
-    <ul class="collection">
+      <ul class="collection">
         <li class="collection-item">Alvin</li>
         <li class="collection-item">Alvin</li>
         <li class="collection-item">Alvin</li>
@@ -75,7 +80,7 @@
     </article>
 
     <article class="col s4">
-    <ul class="collection">
+      <ul class="collection">
         <li class="collection-item">Alvin</li>
         <li class="collection-item">Alvin</li>
         <li class="collection-item">Alvin</li>
